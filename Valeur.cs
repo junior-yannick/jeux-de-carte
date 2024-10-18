a510e01bb1696a7707f760dcd16f05ae51f69ae3
tcar1378 <@tcar1378>
2024-10-18 00:19:10 -0400

    implémentation du code de la Classe Valeur

---
+++ Valeur.cs
@@ -0,0 +1,17 @@
+// Enum pour les Valeurs
+public enum Valeur
+{
+    As = 11,
+    Deux = 2,
+    Trois = 3,
+    Quatre = 4,
+    Cinq = 5,
+    Six = 6,
+    Sept = 7,
+    Huit = 8,
+    Neuf = 9,
+    Dix = 10,
+    Valet = 14,
+    Dame = 26,
+    Roi = 38
+}
